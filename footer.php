43b88159ccb1cf59cf228a3b1f15d1513720d058
hxk7975 <hxk7975@rit.edu>
2016-04-13 16:17:03 -0400

    Danny Kim
    footer,php
    head.php
    index.php
    quantifiers.php
    index.css

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
 <!-- Lauren Johnston: set up basic html 4/4/16-->
+		<footer>
+			<div class="bottomtext">&copy; Danny Kim</div>
+		</footer>
 	</body>
 </html>
--- head.php
+++ head.php
@@ -5,82 +5,21 @@
     <meta charset="UTF-8">
     <title>Regular Expressions | <?=$title?></title>
   	<link rel="stylesheet" href="index.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
   </head>
   <body>
-  	<nav>
-  		<ul>
-  			<li>
-  				<a href="index.php">Introduction</a>
-  			</li>
-  			<li>
-  				<a href="simple.php">Tutorials</a>
-  				<ul>
-  					<li>
-  						<a href="simple.php">Topic 1: Finding Simple Text</a>
-  					</li>
-  					<li>
-  						<a href="sets.php">Topic 2: Character Set</a>
-  					</li>
-  					<li>
-  						<a href="meta.php">Topic 3: Meta Character</a>
-  					</li>
-  					<li>
-  						<a href="quantifiers.php">Topic 4: Quantifier</a>
-  					</li>
-  					<li>
-  						<a href="sub.php">Topic 5: Sub-Expression</a>
-  					</li>
-  					<li>
-  						<a href="back.php">Topic 6: Back References</a>
-  					</li>
-  					<li>
-  						<a href="look.php">Topic 7: Look Around</a>
-  					</li>
-  				</ul>
-  			</li>
-  			<li>
-  				<a href="#">Quizzes</a>
-  				<ul>
-  					<li>
-  						<a href="#">Topic 1: Finding Simple Text</a>
-  					</li>
-  					<li>
-  						<a href="#">Topic 2: Character Set</a>
-  					</li>
-  					<li>
-  						<a href="#">Topic 3: Meta Character</a>
-  					</li>
-  					<li>
-  						<a href="#">Topic 4: Quantifier</a>
-  					</li>
-  					<li>
-  						<a href="#">Topic 5: Sub-Expression</a>
-  					</li>
-  					<li>
-  						<a href="#">Topic 6: Back References</a>
-  					</li>
-  					<li>
-  						<a href="#">Topic 7: Look Around</a>
-  					</li>
-  				</ul>
-  			</li>
-  			<li>
-  				<a href="#">Appendix</a>
-  			</li>
-  			<li>
-  				<a href="extra.php">Extra Help</a>
-  				<ul>
-  					<li>
-  						<a href="test.php">Regular Expressions Tester</a>
-  					</li>
-  					<li>
-  						<a href="extra.php">Common Errors and References</a>
-  					</li>
-  				</ul>
-  			</li>
-  			<li>
-  				<a href="#">About</a>
-  			</li>
-  		</ul>
-  	</nav>
+    <nav>			
+		<div class = "inner-section">
+			<ul class = "main-menu"> 	
+				<li><a href = '#'>Home</a></li>
+				<li><a href = '#'>Tools</a></li>
+				<li><a href = '#'>Tutorials</a></li>
+				<li><a href = '#'>Quizzes</a></li>
+				<li><a href = '#'>Appendix</a></li>
+				<li><a href = '#'>About</a></li>
+			</ul>
+		</div>
+	</nav>	
+	
   	<h1><?=($head ? $head : $title )?></h1>
+	
