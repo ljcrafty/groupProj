87f2455acdf50888d1ab3e289b8391e870a8a367
ljcrafty <ljcrafty@comcast.net>
2016-05-10 18:06:41 -0400

    Changed About Page to Fix Errors and Format
    Changed some spelling errors and formatted the about page

--- about.php
+++ about.php
@@ -4,47 +4,48 @@
 <div id = "about">
 	<div class = "people">
 			<ul>
-				<img src = "img/lauren.jpg" alt = "Lauren" >
-				<li>
+				<li class="about">
+					<img src = "img/lauren.jpg" alt = "Lauren" >
 					<strong>Lauren Johnston</strong><br/>
 					<strong>From:</strong> Lebanon, PA<br/>
 					<strong>Major:</strong> HCC, Web and Mobile Computing<br/>
-					<strong>Fun Fact:</strong> I'm half canadian with citizenship and I'm the tallest girl in my immediate familiy and I am only 5'2<br/>
+					<strong>Fun Fact:</strong> I'm half canadian with citizenship<br/>
 				</li>
 
-				<img src = "img/nicole.jpg" alt = "Nicole" >
-				<li>
+				<li class="about">
+					<img src = "img/nicole.jpg" alt = "Nicole" >
 					<strong>Nicole Ganung</strong><br/>
 					<strong>From:</strong> Hamilton, NY<br/>
 					<strong>Major:</strong> Web and Mobile Computing<br/>
 					<strong>Fun Fact:</strong> I collect road signs and am part of Alpha Xi Delta<br/>
 				</li>
 
-				<img src = "img/danny.jpg" alt = "Danny" >
-				<li>
+				<li class="about">
+					<img src = "img/danny.jpg" alt = "Danny" >
 					<strong>Danny Kim</strong><br/>
 					<strong>From:</strong> Los Angeles, CA<br/>
 					<strong>Major:</strong> Information Technology<br/>
 					<strong>Fun Fact:</strong> I like watching sports, and I can speak Korean fluently!<br/>
 				</li>
 
-				<img src = "img/charles.jpg" alt = "Charles" >
-				<li>
+				<li class="about">
+					<img src = "img/charles.jpg" alt = "Charles" >
 					<strong>Charles Shaner</strong><br/>
 					<strong>From:</strong> Hughesville, PA<br/>
 					<strong>Major:</strong> Information Technology<br/>
 					<strong>Fun Fact:</strong> I can play piano, harmonica, and ocarina. I am very musical<br/>
 				</li>
 
-				<img src = "img/zack.jpg" alt = "Zack" >
-				<li>
+				<li class="about">
+					<img src = "img/zack.jpg" alt = "Zack" >
 					<strong>Zack Ozug</strong><br/>
 					<strong>From:</strong> Albany, NY<br/>
 					<strong>Major:</strong> Web and Mobile Computing<br/>
 					<strong>Fun Fact:</strong> I like to go long walks on the beach<br/>
 				</li>
 			</ul>
-		</div>
-
+	</div>
+</div>
+</div>
 
 <?php require("footer.php");?>
